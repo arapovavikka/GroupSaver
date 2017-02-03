dd615fef5486a23cec9f1acd38e1bf6ad51b066c
vikka <arapova.vikka@yandex.ru>
2017-02-03 10:06:38 +0300

    Added string resourses

--- GroupSaver/GroupSaver/Resources/Resource.Designer.cs
+++ GroupSaver/GroupSaver/Resources/Resource.Designer.cs
@@ -128,6 +128,9 @@ namespace GroupSaver
 			// aapt resource value: 0x7f040001
 			public const int ApplicationName = 2130968577;
 			
+			// aapt resource value: 0x7f040002
+			public const int Favourite = 2130968578;
+			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
 			
